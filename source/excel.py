6ea68aa795bf9013c26f4ccda1705573a2a6dfa7
Gerixmus <gerixmuss@gmail.com>
2024-08-16 22:19:17 +0200

    refactor: Changed directory to save the excel file

--- source/excel.py
+++ source/excel.py
@@ -7,4 +7,4 @@ def get_excel(excursion_destinations):
     colums = ["Name", "Description", "Contact"]
     data = [['=HYPERLINK("%s", "%s")' % (dest.url, dest.name.replace("\"", "'")), dest.description, str.join(", ", dest.contact_info)] for dest in excursion_destinations]
     df = pd.DataFrame(data=data, columns=colums)
-    df.to_excel('destinations.xlsx', index=False)
+    df.to_excel('./output/destinations.xlsx', index=False)
